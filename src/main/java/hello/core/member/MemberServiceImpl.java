572bb3c4c17eaf3dc29e82a0e409da58bc8ded46
Taexee <hee66265@naver.com>
2023-08-15 07:03:37 +0900

    회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,9 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // MemberRepository는 인터페이스를 의존
+    // 실제 할당하는 MemoryMemberRepository가 구현체를 의존
+    // MemberServiceImpl는 추상화, 구체화 모두 의존 DIP 위반
 
     @Override
     public void join(Member member) {
