a7d3ad6c8bc6ec0c5134841950af219e5189f859
Taexee <hee66265@naver.com>
2023-08-25 07:37:07 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -12,12 +14,31 @@ public class AppConfig {
     // 생성한 객체 인스턴스의 참조를 생성자를 통해 주입
     // 구현 객체는 외부(AppConfig)에 의해서 결정
 
+    /*
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+    }
+    */
+
+    // 역할에 따른 구현을 잘보이게 리팩토링
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
     }
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+    // 역활과 구현 클래스를 빠르게 파악 가능
 }
