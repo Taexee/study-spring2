c951750f3728ccf99b406aed5b031d20e8df49f7
Taexee <hee66265@naver.com>
2023-08-29 16:30:32 +0900

    스프링 빈 조회 - 기본

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBeanFindTest.java
@@ -0,0 +1,55 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memberService.getClass() = " + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+        // 같은 타입인 경우 문제가 생김
+        // 인터페이스로 조회하면 구현체가 대상이 됨
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+        // 스프링 컨테이너에 MemberServiceImpl가 등록이 되어있으면 조회 가능
+        // 역할과 구현을 분리하고 역할에 의존 -> 지금 이 테스트는 구현에 의존, 유연성이 떨어진다. 좋은 코드는 아님
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회2")
+    void findBeanByNameX() {
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+        // NoSuchBeanDefinitionException
+        // xxxx라는 빈을 등록한 적이 없기 때문에 예외 발생
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxx", MemberService.class));
+        // ac.getBean을 실행할 때 예외가 발생하면 성공
+    }
+
+}
