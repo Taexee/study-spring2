504a8beae3af8f135dfbeef2dce1bb335022c033
Taexee <hee66265@naver.com>
2023-10-11 18:20:07 +0900

    중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,7 +17,14 @@ import org.springframework.context.annotation.FilterType;
 // ComponentScan -> @Component 애노테이션이 붙은 클래스를 찾아서 알아서 스프링 빈으로 등록해줌
 // 예제코드 유지. AppConfig, TestConfig 등 제외하기 위해 Configuration클래스 excludeFilters 사용해 제외
 public class AutoAppConfig {
-
+    // 수동으로 빈 등록, 자동으로 빈 등록 충돌 테스트
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    // 스프링 : 수동 등록 빈이 우선권 -> 수동 빈이 자동 빈을 오버라이딩 해버림, 버그 발생 가능
+    // 스프링 부트 : 에러 발생, spring.main.allow-bean-definition-overriding=false 를 기본값으로 설정되어 있음
+    //
 }
 
 // AppConfig에서 @Bean 으로 직접 설정정보 작성, 의존관계 명시
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component // memoryMemberRepository 으로 빈에 등록됨
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
