ebd5ae203fa8cb40716a339ff7aaae7f06f0d31f
Taexee <hee66265@naver.com>
2023-10-25 21:17:26 +0900

    InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,15 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     // 접속해야될 서버의 url
     private String url;
 
     // 생성자
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
-
     }
 
     // 외부에서 set
@@ -30,5 +30,20 @@ public class NetworkClient {
     // 서비스 종료
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입이 끝나고 호출
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // 빈이 종료될 때 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
